64f0180ccce7d3d513a9e4166900382ab9acc5fb
Wu Han <wuhanstudio@qq.com>
2019-10-09 16:02:30 +0800

    Fix static variables in U8g2lib.h

--- port/U8g2lib.c
+++ port/U8g2lib.c
@@ -1,5 +1,11 @@
 #include "U8g2lib.h"
 
+static int i2c_device;
+static const char i2c_bus[] = "/dev/i2c-1";
+
+static int spi_device;
+static const char spi_bus[] = "/dev/spidev0.0";
+
 uint8_t u8x8_arm_linux_gpio_and_delay(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
 {
     switch(msg)
