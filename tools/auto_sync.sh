059b9461e6ffb3005779fae4d5dcfae6c403d538
wuhanstudio <wuhanstudio@qq.com>
2021-10-06 10:30:57 +0100

    simplify auto_sync.sh

--- tools/auto_sync.sh
+++ tools/auto_sync.sh
@@ -6,21 +6,4 @@ rm -rf ../csrc/*
 cp u8g2/csrc/* ../csrc
 
 # Generate C++ wrapper
-cd codebuild
-make
-./codebuild
-cp u8g2_d_setup.c u8g2_d_memory.c ../../csrc/
-
-# Remove previous C++ headers
-sed -i '/\/\* generated code (codebuild), u8g2 project \*\//,/#endif \/\/ U8X8_USE_PINS/{//!d}'  ../../cppsrc/U8g2lib.h
-sed -i '/\/\* generated code (codebuild), u8g2 project \*\//,/#endif \/\/ U8X8_USE_PINS/{//!d}'  ../../cppsrc/U8x8lib.h
-
-# Copy C++ headers into file 
-sed -i '/\/\* generated code (codebuild), u8g2 project \*\// r U8g2lib.h' ../../cppsrc/U8g2lib.h
-sed -i '/\/\* generated code (codebuild), u8g2 project \*\// r U8x8lib.h' ../../cppsrc/U8x8lib.h
-
-# Remove the extra line /* generated code (codebuild), u8g2 project */
-sed -i '0,/\/\* generated code (codebuild), u8g2 project \*\// s///' ../../cppsrc/U8g2lib.h
-sed -i '0,/\/\* generated code (codebuild), u8g2 project \*\// s///' ../../cppsrc/U8x8lib.h
-sed -i 'N;s/\n\/\* generated code (codebuild), u8g2 project \*\//\/\* generated code (codebuild), u8g2 project \*\//' ../../cppsrc/U8g2lib.h
-sed -i 'N;s/\n\/\* generated code (codebuild), u8g2 project \*\//\/\* generated code (codebuild), u8g2 project \*\//' ../../cppsrc/U8x8lib.h
+cd codebuild && make install
